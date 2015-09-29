e00ac6fffbf0a33e6cd27355ad63f1498f5e1147
Denis Nurboja <denis.nurboja@drtechno.ba>
2015-09-29 20:10:46 +0200

    removed ENV from files

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe
 	include HTTParty
-	ENV['FOOD2FORK_KEY'] = "33ea4b999695877a4e4be6e0dc792829"
-	key_value = ENV['FOOD2FORK_KEY'] || "33ea4b999695877a4e4be6e0dc792829"
+	key_value = ENV['FOOD2FORK_KEY']
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	base_uri "http://#{hostport}/api/"
     default_params key: key_value
